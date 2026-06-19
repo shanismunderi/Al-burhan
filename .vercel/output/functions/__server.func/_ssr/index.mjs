c95591a3c494d3794e1c03e7bcf210d74ca5c704
afeej <afeej@CIVIL.local>
2026-06-19 19:48:03 +0530

    updated

--- .vercel/output/functions/__server.func/_ssr/index.mjs
+++ .vercel/output/functions/__server.func/_ssr/index.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 let serverEntryPromise;
 async function getServerEntry() {
   if (!serverEntryPromise) {
-    serverEntryPromise = import("./server-DrrGCU9u.mjs").then((n) => n.s).then(
+    serverEntryPromise = import("./server-93GtQvW5.mjs").then((n) => n.aa).then(
       (m) => m.default ?? m
     );
   }
